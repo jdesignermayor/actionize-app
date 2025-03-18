5af30f721ff4f7dae174c4b7cea1084d00cb1057
joseluis <jdesignermayor@gmail.com>
2025-03-18 13:48:51 -0500

    fix: navbar tasks responsive fix

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export default function Nav() {
   return (
-    <div className="flex items-center justify-between h-[60px] py-6 lg:px-[calc(5%)]">
+    <div className="flex items-center justify-between h-[60px] py-6 px-14">
       <div className="flex items-center gap-2">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
           <path fillRule="evenodd" d="M6.32 2.577a49.255 49.255 0 0 1 11.36 0c1.497.174 2.57 1.46 2.57 2.93V21a.75.75 0 0 1-1.085.67L12 18.089l-7.165 3.583A.75.75 0 0 1 3.75 21V5.507c0-1.47 1.073-2.756 2.57-2.93Z" clipRule="evenodd" />
--- app/components/task.tsx
+++ app/components/task.tsx
@@ -10,7 +10,7 @@ export default function Task({ name }: TaskType) {
         {isEditMode ? <div className="flex flex-col gap-3 items-left p-5 cursor-pointer bg-black hover:border-b-[#dfdfdf] border-b border-b-[#444444]" onClick={() => setIsEditMode(false)}>
             edit mode enabled
             <p>{name}</p>
-        </div> : <div tabIndex={0} className="flex flex-col gap-3 items-left p-5 cursor-pointer hover:font-bold hover:border-b-[#dfdfdf] border-b border-b-[#444444] text-xl" onClick={() => setIsEditMode(true)}>
+        </div> : <div tabIndex={0} className="flex flex-col gap-3 items-left p-5 cursor-pointer hover:font-bold hover:border-b-[#dfdfdf] border-b border-b-[#444444] text-lg" onClick={() => setIsEditMode(true)}>
             <p>{name}</p>
         </div>}
     </>
